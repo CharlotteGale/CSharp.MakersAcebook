346ac58907629338e1dc28a7febe2f5492d090df
Charlotte Gale <CharlotteMGale@outlook.com>
2026-02-09 15:26:49 +0000

    delete appsettings.development ready for secure secrets

--- Acebook/Models/AcebookDbContext.cs
+++ Acebook/Models/AcebookDbContext.cs
@@ -28,7 +28,7 @@ public class AcebookDbContext : DbContext
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql(@"Host=localhost;Username=postgres;Password=1234;Database=" + GetDatabaseName());
+        => optionsBuilder.UseNpgsql(@"Host=localhost;Username=charlottegale;Password=1234;Database=" + GetDatabaseName());
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
