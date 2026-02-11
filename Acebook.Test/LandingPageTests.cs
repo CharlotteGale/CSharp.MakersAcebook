0dbe5cc3ede0200777210d34f9c94b66946f1aae
Rachelllk97 <rachelkellyprod@gmail.com>
2026-02-11 16:55:30 +0000

    basic tests for log in and sign up added

--- Acebook.Test/LandingPageTests.cs
+++ Acebook.Test/LandingPageTests.cs
@@ -1,4 +1,5 @@
 namespace Acebook.Test;
+using System.Text.RegularExpressions;
 
 public class LandingPageTests : PageTest
 {
@@ -9,4 +10,31 @@ public class LandingPageTests : PageTest
 
     await Expect(Page).ToHaveTitleAsync("- Acebook");
   }
+
+
+  [Test]
+  public async Task LandingPage_Show_Login_Form()
+  {
+    await Page.GotoAsync("http://127.0.0.1:5287");
+
+    var form = Page.Locator("form.login-card");
+    await Expect(form).ToBeVisibleAsync();
+  }
+
+
+[Test]
+public async Task User_Can_Navigate_To_SignUp_Page()
+{
+
+    await Page.GotoAsync("http://127.0.0.1:5287");
+
+    await Page.ClickAsync(".new-acc-btn");
+
+    await Expect(Page).ToHaveURLAsync(new Regex(".*/signup", RegexOptions.IgnoreCase));
+
+    await Expect(Page.Locator("#email")).ToBeVisibleAsync();
+    await Expect(Page.Locator("#password")).ToBeVisibleAsync();
+    await Expect(Page.Locator("#name")).ToBeVisibleAsync();
+}
+
 }
