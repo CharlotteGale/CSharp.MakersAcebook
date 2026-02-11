e1527865a152bd8f1b59606467d4eaffa1b71ed2
Charlotte Gale <CharlotteMGale@outlook.com>
2026-02-11 12:13:08 +0000

    change test suite to Playwright and updates CONFIG_README for Playwright instructions

--- Acebook.Test/LandingPageTests.cs
+++ Acebook.Test/LandingPageTests.cs
@@ -1,32 +1,12 @@
 namespace Acebook.Test;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium;
 
-public class LandingPageTests
+public class LandingPageTests : PageTest
 {
-  ChromeDriver driver;
-
-  [SetUp]
-  public void Setup()
-  {
-      driver = new ChromeDriver();
-  }
-
-  [TearDown]
-  public void TearDown()
-  {
-      if (driver != null)
-      {
-          driver.Quit();
-          driver.Dispose();
-      }
-  }
-
   [Test]
-  public void LandingPage_ShowsWelcomeMessage()
+  public async Task LandingPage_ShowsWelcomeMessage()
   {
-    driver.Navigate().GoToUrl("http://127.0.0.1:5287");
-    IWebElement greeting = driver.FindElement(By.Id("greeting"));
-    Assert.That(greeting.GetAttribute("innerHTML"), Is.EqualTo("Welcome To Acebook"));
+    await Page.GotoAsync("http://127.0.0.1:5287");
+
+    await Expect(Page).ToHaveTitleAsync("- acebook");
   }
 }
