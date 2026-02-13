bc93fc0a26283da438c8a246031c9c37a378038e
Charlotte Gale <CharlotteMGale@outlook.com>
2026-02-13 13:48:16 +0000

    adds modelstate validation and json voodoo magic across Posts & Feed controllers

--- Acebook/Controllers/PostsController.cs
+++ Acebook/Controllers/PostsController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using acebook.Models;
 using acebook.ActionFilters;
+using System.Text.Json;
 
 namespace acebook.Controllers;
 
@@ -20,7 +21,6 @@ public class PostsController : Controller
     [Route("/Posts")]
     [HttpGet]
     public IActionResult Index() {
-      // AcebookDbContext dbContext = new AcebookDbContext();
       List<Post> posts = _context.Posts.ToList();
       ViewBag.Posts = posts;
       return View();
@@ -29,6 +29,34 @@ public class PostsController : Controller
     [Route("/Posts")]
     [HttpPost]
     public RedirectResult Create(Post post) {
+      
+      ModelState.Remove("UserId");
+      ModelState.Remove("User");
+
+      if (!ModelState.IsValid)
+      {
+        // Post conversion
+        TempData["InvalidPost"] = JsonSerializer.Serialize(post);
+        /// <summary>
+        /// This takes a Post object and converts it to a JSON string.
+        /// Stores it into TempData, which survives one redirect.
+        /// </summary>
+        
+        // Validation error extraction and conversion
+        TempData["ModelStateErrors"] = JsonSerializer.Serialize(
+          ModelState.Where(x => x.Value.Errors.Count > 0)
+            .ToDictionary(
+              kvp => kvp.Key,
+              kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray()
+            )
+        );
+        /// <summary>
+        /// Captures the errors found in ModelState, converts it to a dictionary.
+        /// Which is then converted to JSON and stored in TempData.
+        /// </summary>
+        return new RedirectResult("/Feed");
+      }
+
       int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
       post.UserId = currentUserId;
       _context.Posts.Add(post);
