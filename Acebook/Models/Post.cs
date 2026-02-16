e77aba17d771cf79cc20d023f51f1e1293a319b7
Rachelllk97 <rachelkellyprod@gmail.com>
2026-02-16 10:42:42 +0000

    added datetime to posts model

--- Acebook/Models/Post.cs
+++ Acebook/Models/Post.cs
@@ -9,8 +9,8 @@ public class Post
   public string Content {get; set;} = null!;
   public int UserId {get; set;}
   public User User {get; set;} = null!;
+  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
   public ICollection<Comment> Comments {get; set;} = new List<Comment>();
-
   public ICollection<Like> Likes {get; set;} = new List<Like>();
   
   }
